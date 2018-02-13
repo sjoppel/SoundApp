e561106bd2f97bdd493b91eb1ffbbd25da8ecc64
sjoppel <sjoppel@me.com>
2018-02-13 09:42:42 -0600

    test

--- app/src/main/java/com/example/oppelsa/soundapp/MainActivity.java
+++ app/src/main/java/com/example/oppelsa/soundapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.oppelsa.soundapp;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sound (View view) {
+        MediaPlayer piano = MediaPlayer.create(getApplicationContext(), R.raw.piano);
+        piano.start();
+    }
+
+    public void message(View view) {
+        Intent myIntent = new Intent(this, Secondary.class);
+        startActivity(myIntent);
+    }
+
+    public void email(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        //Below adds to the email to me
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"sjoppel@gmail.com"}); // recipients
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Sound App");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Listen to my groovy sound!");
+
+        startActivity(emailIntent);
     }
 }
